6b8dac73166ddc8aa403b6a345d56f2448476e59
wadim55 <korkin_wadim@mail.ru>
2023-09-02 19:53:25 +0600

    ограничители для шашки

--- Assets/Shashka.cs
+++ Assets/Shashka.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Shashka : MonoBehaviour, IDragHandler, IDropHandler, IBeginDragHandler
 {
     [SerializeField] private GameObject grid;   //ссылка на Сетку, чтобы получить доступ к координатам ячеек доски
     private Vector2 _startPosition; // позиция шашки, перед тем как её потянули
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+    
     
     #region Drag работает когда тянем шашку
     public void OnDrag(PointerEventData eventData)
@@ -27,7 +34,7 @@ public class Shashka : MonoBehaviour, IDragHandler, IDropHandler, IBeginDragHand
         {
             for (var j = 0; j < grid.GetComponent<Grid>()._cell.GetLength(1); j++)
             {
-                if ((Input.mousePosition - grid.GetComponent<Grid>()._cell[i, j].position).magnitude < 30)
+                if ((Input.mousePosition - grid.GetComponent<Grid>()._cell[i, j].position).magnitude < 30 && grid.GetComponent<Grid>()._cell[i, j].gameObject.GetComponent<Image>().color == Color.black && (_startPosition - (Vector2)grid.GetComponent<Grid>()._cell[i, j].position).magnitude < 150 && _startPosition.y - grid.GetComponent<Grid>()._cell[i, j].position.y < 0)
                 {
                     transform.position = grid.GetComponent<Grid>()._cell[i, j].position;
                     return;
