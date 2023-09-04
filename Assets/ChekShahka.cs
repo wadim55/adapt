e61033d2e7a298a0b1b12f9bce0dab9f74bd3cdf
KorkinIgor <igorkorkin55@gmail.com>
2023-09-04 16:15:06 +0600

    Добавил черные шашки

--- Assets/ChekShahka.cs
+++ Assets/ChekShahka.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class ChekShahka : MonoBehaviour
 {
    [SerializeField] private GameObject[] Shahka;
+   [SerializeField] private GameObject[] ShahkaBlack;
    [SerializeField] private Grid _grid;
 
    private void Start()
@@ -22,5 +23,17 @@ public class ChekShahka : MonoBehaviour
       Shahka[9].transform.position = _grid._cell[5, 2].position;
       Shahka[10].transform.position = _grid._cell[5, 4].position;
       Shahka[11].transform.position = _grid._cell[5, 6].position;
+      ShahkaBlack[0].transform.position = _grid._cell[0, 1].position;
+      ShahkaBlack[1].transform.position = _grid._cell[0, 3].position;
+      ShahkaBlack[2].transform.position = _grid._cell[0, 5].position;
+      ShahkaBlack[3].transform.position = _grid._cell[0, 7].position;
+      ShahkaBlack[4].transform.position = _grid._cell[1, 0].position;
+      ShahkaBlack[5].transform.position = _grid._cell[1, 2].position;
+      ShahkaBlack[6].transform.position = _grid._cell[1, 4].position;
+      ShahkaBlack[7].transform.position = _grid._cell[1, 6].position;
+      ShahkaBlack[8].transform.position = _grid._cell[2, 1].position;
+      ShahkaBlack[9].transform.position = _grid._cell[2, 3].position;
+      ShahkaBlack[10].transform.position = _grid._cell[2, 5].position;
+      ShahkaBlack[11].transform.position = _grid._cell[2, 7].position;
    }
 }
